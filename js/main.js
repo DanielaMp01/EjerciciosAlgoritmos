97ae5c3fb55cdaf382caf01765f988a6d3f28a61
daniela <danielam.morenop@gmail.com>
2022-05-25 16:44:13 -0500

    Primer commit de los ejercicios de Javascript

---
+++ js/main.js
@@ -0,0 +1,75 @@
+//EJERCICIO 1
+// const num1 = parseFloat(prompt(" Ingresa el primer numero del 1 al 100"));
+// const num2 = parseFloat(prompt(" Ingresa el segundo numero del 1 al 100"));
+// const num3 = parseFloat(prompt(" Ingresa el tercer numero del 1 al 100"));
+// let num_mayor;
+// if(num1 >= num2 && num1 >= num3) {
+//     num_mayor= num1;
+// }
+// else if (num2 >= num1 && num2 >= num3) {
+//     num_mayor =  num2;
+// }
+// else {
+//     num_mayor = num3;
+// }
+// console.log("El número mayor de los 3 números ingresados es: " + num_mayor);
+
+//EJERCICIO 2
+// let num_menor;
+// if(num1 <= num2 && num1 <= num3) {
+//      num_menor = num1;
+// }
+// else if (num2 <= num1 && num2 <= num3) {
+//     num_menor =  num2;
+// }
+// else {
+//     num_menor = num3;
+// }
+// console.log("El número menor de los 3 números ingresados es: " + num_menor);
+
+
+// //EJERCICIO 4
+// const num1 = parseInt(prompt(" Ingresa el primer número "));
+// const num2 = parseInt(prompt(" Ingresa el segundo número "));
+// const num3 = parseInt(prompt(" Ingresa el tercer número "));
+// let suma;
+// if(num1 + num2 == num3) {
+//      suma= num1 + num2;
+//  }
+// else if (num2 + num3 == num1){
+//      suma =  num2 + num3; {
+// else if (num2 >= num1 && num2 >= num3) {
+//         num_mayor =  num2;
+//  }
+//  else {
+//     num_mayor = num3;
+//  }}}
+// console.log("El número mayor de los 3 números ingresados es: " + num_mayor);
+
+
+const num1 = parseInt(prompt(" Ingresa un número entero entre el 100 y el 200"));
+if(num1<100 || num1>200){
+    alert(parseInt(prompt(" El numero ingresado no es un número entero entre el 100 y el 200")));
+  }
+     else if(( num1 % 3 ) == 0){
+        alert(num1 + ' si es multiplo de 3');
+  }else{
+    
+        alert(num1 + ' no es multiplo de 3');
+    }
+   
+
+
+
+//EJERCICIO 6
+    // const num1 = parseInt(prompt(" Ingresa un número entero"));
+    //        if(( num1 % 2 ) == 0){
+        
+    //         alert(num1 + ' es par');
+            
+    //     }else{
+        
+    //         alert(num1 + ' es impar');
+    //     }
+    
+    
